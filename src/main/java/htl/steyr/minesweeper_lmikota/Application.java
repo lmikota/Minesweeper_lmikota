8452ab904a41b2607010c5d0150288ab9d808f10
Leander Mikota <lmikota@htl-steyr.ac.at>
2025-01-10 11:16:30 +0100

    Deleted FXML File, added a Menu to choose difficulty, added Functions to generate MinesweeperField and to set red flags when right clicking a button

--- src/main/java/htl/steyr/minesweeper_lmikota/Application.java
+++ src/main/java/htl/steyr/minesweeper_lmikota/Application.java
@@ -12,11 +12,18 @@
  */
 package htl.steyr.minesweeper_lmikota;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -26,22 +33,31 @@ public class Application extends javafx.application.Application {
 
 
     private String greenImagePath = "";
+    private String difficultySelected = "";
     private int rows = 8;
     private int columns = 8;
 
     @Override
     public void start(Stage stage) throws IOException {
-        GridPane gridPane = new GridPane();
+        HBox difficultyOptionPane = new HBox();
+        difficultyOptionPane.setAlignment(Pos.CENTER);
+        Text text = new Text("Choose Difficulty");
 
-        for (int row = 0; row < getRows(); row++) {
-            for (int col = 0; col < getColumns(); col++) {
-                setGreenImagePath("/htl/steyr/minesweeper_lmikota/img/Solid_green.png");
-                Button button = createMinesweeperButton(getGreenImagePath());
-                gridPane.add(button, col, row);
-            }
-        }
+        Button difficultyEasyButton = new Button("Easy");
+        Button difficultyMediumButton = new Button("Medium");
+        Button difficultyHardButton = new Button("Hard");
 
-        Scene scene = new Scene(gridPane);
+        difficultyOptionPane.getChildren().addAll(text,difficultyEasyButton,difficultyMediumButton,difficultyHardButton);
+
+        difficultyHardButton.setPrefSize(100,50);
+        difficultyEasyButton.setPrefSize(100,50);
+        difficultyMediumButton.setPrefSize(100,50);
+        difficultyEasyButton.setOnAction(actionEvent -> createMinesweeperField("Easy",stage));
+        difficultyMediumButton.setOnAction(actionEvent -> createMinesweeperField("Medium",stage));
+        difficultyHardButton.setOnAction(actionEvent -> createMinesweeperField("Hard",stage));
+
+
+        Scene scene = new Scene(difficultyOptionPane);
         stage.setTitle("Minesweeper");
         stage.setResizable(true);
         stage.setScene(scene);
@@ -53,7 +69,7 @@ public class Application extends javafx.application.Application {
     }
 
     /**
-     * This Method creates a Button with an Image insode of it
+     * This Method creates a Button with an Image inside of it
      *
      * @param    imagePath The Path of the image which will be loaded into the button
      *
@@ -62,17 +78,72 @@ public class Application extends javafx.application.Application {
     private Button createMinesweeperButton(String imagePath) {
         URL imageUrl = getClass().getResource(imagePath);
         if (imageUrl != null) {
-            Image image = new Image(imageUrl.toString());
-            ImageView imageView = new ImageView(image);
-            imageView.setFitHeight(35);
-            imageView.setFitWidth(35);
-            Button button = new Button("", imageView);
-            button.setStyle("-fx-background-color: transparent; -fx-border-color: transparent;");
+//            Image image = new Image(imageUrl.toString());
+//            ImageView imageView = new ImageView(image);
+//            imageView.setFitHeight(35);
+//            imageView.setFitWidth(35);
+//            Button button = new Button("", imageView);
+            Button button = new Button();
+//            button.setStyle("-fx-background-color: transparent; -fx-border-color: transparent;");
+            button.setOnMouseClicked((MouseEvent event) -> {
+                if (event.getButton() == MouseButton.SECONDARY) {
+                    button.setText("\uD83D\uDEA9");
+                }
+            });
+            button.setOnMouseClicked((MouseEvent event) -> {
+                if (event.getButton() == MouseButton.PRIMARY) {
+                    /**
+                     * @ToDo
+                     *
+                     *Methode Aufrufen die den Button wegmacht und das Feld aufdeckt
+                     */
+                }
+            });
             return button;
         } else {
             System.err.println("Image not found: " + imagePath);
             return new Button("Image Error");
         }
+    }
+
+    /**
+     * This Method creates MinesweeperField depending on the difficulty
+     *
+     * @param    difficulty The selected difficulty the MinesweeperField will be loaded in
+     * @param    stage The stage reference
+     *
+     */
+    private void createMinesweeperField (String difficulty, Stage stage) {
+        setDifficultySelected(difficulty);
+        GridPane minesweeperPane = new GridPane();
+        switch (difficulty) {
+            case "Easy":
+                setRows(8);
+                setColumns(8);
+                break;
+            case "Medium":
+                setRows(16);
+                setColumns(16);
+                break;
+            case "Hard":
+                setRows(16);
+                setColumns(30);
+                break;
+        }
+
+        for (int row = 0; row < getRows(); row++) {
+            for (int col = 0; col < getColumns(); col++) {
+                setGreenImagePath("/htl/steyr/minesweeper_lmikota/img/Solid_green.png");
+                Button button = createMinesweeperButton(getGreenImagePath());
+                minesweeperPane.add(button, col, row);
+            }
+        }
+
+        Scene scene = new Scene(minesweeperPane);
+        stage.setTitle("Minesweeper");
+        stage.setResizable(true);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public String getGreenImagePath() {
@@ -98,4 +169,13 @@ public class Application extends javafx.application.Application {
     public void setColumns(int columns) {
         this.columns = columns;
     }
+
+    public String getDifficultySelected() {
+        return difficultySelected;
+    }
+
+    public void setDifficultySelected(String difficultySelected) {
+        this.difficultySelected = difficultySelected;
+    }
+
 }
