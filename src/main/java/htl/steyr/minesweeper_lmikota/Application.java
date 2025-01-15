c55c2f61cf8897ec5d050bc4f27f635a13f1bbf4
Leander Mikota <lmikota@htl-steyr.ac.at>
2025-01-15 16:27:54 +0100

    Pasted Methods that were in Application.java to Controller.java, extended Metods to create Bombs randomly on the fields. If you click on buttons their style will change, if you click on a button with a bomb in it the style will change (Skull symbol on the button)
    Deleted Solid_green.png as i do not use it anymore

--- src/main/java/htl/steyr/minesweeper_lmikota/Application.java
+++ src/main/java/htl/steyr/minesweeper_lmikota/Application.java
@@ -19,10 +19,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -31,14 +29,9 @@ import java.net.URL;
 
 public class Application extends javafx.application.Application {
 
-
-    private String greenImagePath = "";
-    private String difficultySelected = "";
-    private int rows = 8;
-    private int columns = 8;
-
     @Override
     public void start(Stage stage) throws IOException {
+        Controller controller = new Controller();
         HBox difficultyOptionPane = new HBox();
         difficultyOptionPane.setAlignment(Pos.CENTER);
         Text text = new Text("Choose Difficulty");
@@ -52,9 +45,9 @@ public class Application extends javafx.application.Application {
         difficultyHardButton.setPrefSize(100,50);
         difficultyEasyButton.setPrefSize(100,50);
         difficultyMediumButton.setPrefSize(100,50);
-        difficultyEasyButton.setOnAction(actionEvent -> createMinesweeperField("Easy",stage));
-        difficultyMediumButton.setOnAction(actionEvent -> createMinesweeperField("Medium",stage));
-        difficultyHardButton.setOnAction(actionEvent -> createMinesweeperField("Hard",stage));
+        difficultyEasyButton.setOnAction(actionEvent -> controller.createMinesweeperField("Easy",stage));
+        difficultyMediumButton.setOnAction(actionEvent -> controller.createMinesweeperField("Medium",stage));
+        difficultyHardButton.setOnAction(actionEvent -> controller.createMinesweeperField("Hard",stage));
 
 
         Scene scene = new Scene(difficultyOptionPane);
@@ -68,114 +61,7 @@ public class Application extends javafx.application.Application {
         launch();
     }
 
-    /**
-     * This Method creates a Button with an Image inside of it
-     *
-     * @param    imagePath The Path of the image which will be loaded into the button
-     *
-     * @return Button
-     */
-    private Button createMinesweeperButton(String imagePath) {
-        URL imageUrl = getClass().getResource(imagePath);
-        if (imageUrl != null) {
-//            Image image = new Image(imageUrl.toString());
-//            ImageView imageView = new ImageView(image);
-//            imageView.setFitHeight(35);
-//            imageView.setFitWidth(35);
-//            Button button = new Button("", imageView);
-            Button button = new Button();
-//            button.setStyle("-fx-background-color: transparent; -fx-border-color: transparent;");
-            button.setOnMouseClicked((MouseEvent event) -> {
-                if (event.getButton() == MouseButton.SECONDARY) {
-                    button.setText("\uD83D\uDEA9");
-                }
-            });
-            button.setOnMouseClicked((MouseEvent event) -> {
-                if (event.getButton() == MouseButton.PRIMARY) {
-                    /**
-                     * @ToDo
-                     *
-                     *Methode Aufrufen die den Button wegmacht und das Feld aufdeckt
-                     */
-                }
-            });
-            return button;
-        } else {
-            System.err.println("Image not found: " + imagePath);
-            return new Button("Image Error");
-        }
-    }
 
-    /**
-     * This Method creates MinesweeperField depending on the difficulty
-     *
-     * @param    difficulty The selected difficulty the MinesweeperField will be loaded in
-     * @param    stage The stage reference
-     *
-     */
-    private void createMinesweeperField (String difficulty, Stage stage) {
-        setDifficultySelected(difficulty);
-        GridPane minesweeperPane = new GridPane();
-        switch (difficulty) {
-            case "Easy":
-                setRows(8);
-                setColumns(8);
-                break;
-            case "Medium":
-                setRows(16);
-                setColumns(16);
-                break;
-            case "Hard":
-                setRows(16);
-                setColumns(30);
-                break;
-        }
 
-        for (int row = 0; row < getRows(); row++) {
-            for (int col = 0; col < getColumns(); col++) {
-                setGreenImagePath("/htl/steyr/minesweeper_lmikota/img/Solid_green.png");
-                Button button = createMinesweeperButton(getGreenImagePath());
-                minesweeperPane.add(button, col, row);
-            }
-        }
-
-        Scene scene = new Scene(minesweeperPane);
-        stage.setTitle("Minesweeper");
-        stage.setResizable(true);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public String getGreenImagePath() {
-        return greenImagePath;
-    }
-
-    public void setGreenImagePath(String greenImagePath) {
-        this.greenImagePath = greenImagePath;
-    }
-
-    public int getRows() {
-        return rows;
-    }
-
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
-    public int getColumns() {
-        return columns;
-    }
-
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
-    public String getDifficultySelected() {
-        return difficultySelected;
-    }
-
-    public void setDifficultySelected(String difficultySelected) {
-        this.difficultySelected = difficultySelected;
-    }
 
 }
--- src/main/java/htl/steyr/minesweeper_lmikota/Controller.java
+++ src/main/java/htl/steyr/minesweeper_lmikota/Controller.java
@@ -1,6 +1,157 @@
 package htl.steyr.minesweeper_lmikota;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.util.Random;
 
 public class Controller {
+    private String difficultySelected = "";
+    private int rows = -1;
+    private int columns = -1;
+    private int totalBombs = 0;
+    private boolean marked = false;
+    private boolean revealed = false;
+    private boolean[][] bombGrid;
 
+    /**
+     * This Method creates a Button with a colored background
+     *
+     * @return Button
+     */
+    public Button createMinesweeperButton(int row, int col) {
+        Button button = new Button();
+        button.setPrefSize(35, 35);
+        button.setStyle("-fx-background-color: lightgreen; -fx-border-color: black;");
+        button.setOnMouseClicked((MouseEvent event) -> {
+            if (event.getButton() == MouseButton.SECONDARY) {
+                if (!button.getText().equals("\uD83D\uDEA9") && !button.getText().equals("\uD83D\uDC80")) {
+                    button.setText("\uD83D\uDEA9");
+                    setMarked(true);
+                } else {
+                    button.setText("");
+                    setMarked(false);
+                }
+            } else if (event.getButton() == MouseButton.PRIMARY && !marked) {
+                if (!"\uD83D\uDEA9".equals(button.getText())) {
+                    if (bombGrid[row][col]) {
+                        button.setStyle("-fx-background-color: white; -fx-border-color: black;");
+                        button.setText("\uD83D\uDC80");
+                    } else {
+                        button.setStyle("-fx-background-color: white; -fx-border-color: black;");
+                    }
+                    setRevealed(true);
+                }
+            }
+        });
+
+        return button;
+    }
+
+    /**
+     * This Method creates MinesweeperField depending on the difficulty
+     *
+     * @param difficulty The selected difficulty the MinesweeperField will be loaded in
+     * @param stage      The stage reference
+     */
+    public void createMinesweeperField(String difficulty, Stage stage) {
+        setDifficultySelected(difficulty);
+        GridPane minesweeperPane = new GridPane();
+        switch (difficulty) {
+            case "Easy":
+                setRows(8);
+                setColumns(8);
+                totalBombs = 10;
+                break;
+            case "Medium":
+                setRows(16);
+                setColumns(16);
+                totalBombs = 40;
+                break;
+            case "Hard":
+                setRows(16);
+                setColumns(30);
+                totalBombs = 99;
+                break;
+        }
+
+        bombGrid = new boolean[getRows()][getColumns()];
+        placeBombs();
+
+        for (int row = 0; row < getRows(); row++) {
+            for (int col = 0; col < getColumns(); col++) {
+                Button button = createMinesweeperButton(row, col);
+
+                minesweeperPane.add(button, col, row);
+                minesweeperPane.setPrefSize(500, 500);
+            }
+        }
+        Scene scene = new Scene(minesweeperPane);
+        stage.setTitle("Minesweeper - " + difficulty);
+        stage.setResizable(true);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Places bombs randomly on the grid.
+     */
+    private void placeBombs() {
+        Random random = new Random();
+        int placedBombs = 0;
+
+        while (placedBombs < totalBombs) {
+            int row = random.nextInt(getRows());
+            int col = random.nextInt(getColumns());
+
+            if (!bombGrid[row][col]) {
+                bombGrid[row][col] = true;
+                placedBombs++;
+            }
+        }
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public String getDifficultySelected() {
+        return difficultySelected;
+    }
+
+    public void setDifficultySelected(String difficultySelected) {
+        this.difficultySelected = difficultySelected;
+    }
+
+    public boolean isMarked() {
+        return marked;
+    }
+
+    public void setMarked(boolean marked) {
+        this.marked = marked;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public void setRevealed(boolean revealed) {
+        this.revealed = revealed;
+    }
 }
